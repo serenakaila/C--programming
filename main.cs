df313b04f23ab462d4a5460b3923f25bdbd8611e
serenakaila <115103250+serenakaila@users.noreply.github.com>
2022-10-05 14:01:48 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,46 @@
+// program to add two numbers; single line
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int a;   //Declaration 
+    int b;   //Declaration
+    int c;   //Declaration
+    int d;   //Declaration
+    int e;   //Declaration
+    int f;   //Declaration
+    int g;   //Declaration
+    int h;   //Declaration
+
+    
+    int answer;  //Declaration
+    a=10;
+    b=18;  //initialization
+    answer= a+b;
+    
+    int answer1;  //Declaration
+    c=30;
+    d=20;  //initialization
+    answer1=c-d;
+    
+    int answer2;  //Declaration
+    e=3;
+    f=8;   //initialization
+    answer2=e*f;
+    
+    int answer3;  //Declaration
+    g=5;
+    h=2;   //initilization
+    answer3=g/h;
+
+    int answer4;  //Declaration
+    answer4=g%h;
+    
+    
+    Console.WriteLine (answer);
+    Console.WriteLine (answer1);
+    Console.WriteLine (answer2);
+    Console.WriteLine (answer3);
+    Console.WriteLine (answer4);
+  }
+}
