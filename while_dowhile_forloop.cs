46f2099fda99ced3be089e39a43451cf65b019e4
serenakaila <115103250+serenakaila@users.noreply.github.com>
2022-11-01 13:36:39 -0400

    Create while_dowhile_forloop.cs

---
+++ while_dowhile_forloop.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int i; //initialization
+    for(i=2; i<21; i=i+2)
+
+    
+    {
+      
+    Console.WriteLine (i);
+    } 
+    //Console.WriteLine ("Hello World");
+  }
+}
+
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int i=2; //initialization
+    do //condition
+    {
+      
+    Console.WriteLine (i);
+      i=i+2; //increment
+    } while(i<21);
+    
+
+
+
+
+
+
+    //Console.WriteLine ("Hello World")
+    
+  }
+}
+
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int i=2; //initialization
+    while(i<21) //conditipn
+    {
+      
+    Console.WriteLine (i);
+    i=i+2; //increment
+    } 
+  }
+}
