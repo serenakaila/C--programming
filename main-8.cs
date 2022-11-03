386a1f1bfefbdf91f4f795e195f0c9442ca86216
serenakaila <115103250+serenakaila@users.noreply.github.com>
2022-11-03 14:01:41 -0400

    Add files via upload

---
+++ main-8.cs
@@ -0,0 +1,120 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("What is integer");
+
+        Console.WriteLine("1. bool");
+        Console.WriteLine("2. int");
+        Console.WriteLine("3. string");
+        Console.WriteLine("4. ifelse");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt = Convert.ToInt32(Console.ReadLine());
+
+        if (opt == 2)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+
+      Console.WriteLine("What is the modulo symbol");
+
+        Console.WriteLine("1. #");
+        Console.WriteLine("2. &");
+        Console.WriteLine("3. *");
+        Console.WriteLine("4. %");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt1 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt1 == 4)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+
+      Console.WriteLine("What is the syntax for do while loops");
+
+        Console.WriteLine("1. do while");
+        Console.WriteLine("2. for loop");
+        Console.WriteLine("3. while");
+        Console.WriteLine("4. while do");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt3 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt3 == 1)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+
+
+      Console.WriteLine("What is the multiplication symbol");
+
+        Console.WriteLine("1. #");
+        Console.WriteLine("2. &");
+        Console.WriteLine("3. *");
+        Console.WriteLine("4. %");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt4 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt4 == 3)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+
+      Console.WriteLine("What is Console.WriteLine used for?");
+
+        Console.WriteLine("1. to show input and output structures");
+        Console.WriteLine("2. to show decimals");
+        Console.WriteLine("3. to show integers");
+        Console.WriteLine("4. to show bool");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt5 = Convert.ToInt32(Console.ReadLine());
+
+        if (opt5 == 1)
+        {
+            Console.WriteLine("Correct");
+        }
+      else
+      {
+                Console.WriteLine("Incorrect");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
